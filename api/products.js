246548f6faed4e6b10d7ba445e5b208532eb6339
fccJashCoda <personalrevolution2014@gmail.com>
2021-02-24 23:21:14 +0100

    Add: get one product

--- api/products.js
+++ api/products.js
@@ -7,4 +7,18 @@ router.get('/', (req, res) => {
   queries.getAll().then((products) => res.json(products));
 });
 
+router.get('/:id', (req, res, next) => {
+  if (isNaN(req.params.id)) {
+    const error = new Error('Invalid id');
+    return next(error);
+  }
+  queries.getOne(req.params.id).then((product) => {
+    if (product) {
+      res.json(product);
+    } else {
+      next();
+    }
+  });
+});
+
 module.exports = router;
