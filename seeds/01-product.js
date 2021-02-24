0d6915989955ae52b5a4f7cb7b4724b26a064cad
fccJashCoda <personalrevolution2014@gmail.com>
2021-02-24 21:32:02 +0100

    add product seeds

---
+++ seeds/01-product.js
@@ -0,0 +1,11 @@
+const products = require('../products');
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('product')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('product').insert(products);
+    });
+};
