ad2e97770c2096c00193bf18998b509e850b67af
fccJashCoda <personalrevolution2014@gmail.com>
2021-02-25 20:18:25 +0100

    Add: create product route

--- api/products.js
+++ api/products.js
@@ -3,6 +3,14 @@ const queries = require('../db/queries');
 
 const router = express.Router();
 
+function validateProduct(product) {
+  if (!(typeof product.title == 'string' && product.title.trim() != ''))
+    return false;
+  if (isNaN(product.price) && product.price > 0) return false;
+  if (isNaN(product.quantity)) return false;
+  return true;
+}
+
 router.get('/', (req, res) => {
   queries.getAll().then((products) => res.json(products));
 });
@@ -19,6 +27,27 @@ router.get('/:id', (req, res, next) => {
       next();
     }
   });
+});
+
+router.post('/', (req, res, next) => {
+  if (validateProduct(req.body)) {
+    const { title, description, price, quantity, image } = req.body;
+
+    const product = {
+      title,
+      description,
+      price,
+      quantity,
+      image,
+    };
+
+    queries.create(product).then((id) => {
+      res.json({ id });
+    });
+  } else {
+    const error = new Error('Invalid product');
+    next(error);
+  }
 });
 
 module.exports = router;
