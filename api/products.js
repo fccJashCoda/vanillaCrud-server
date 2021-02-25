44a132964b1e5fbf18cd8bfb4d4bc6d9ba7a6f8d
fccJashCoda <personalrevolution2014@gmail.com>
2021-02-25 23:53:53 +0100

    Add update product route

--- api/products.js
+++ api/products.js
@@ -3,23 +3,52 @@ const queries = require('../db/queries');
 
 const router = express.Router();
 
-function validateProduct(product) {
-  if (!(typeof product.title == 'string' && product.title.trim() != ''))
-    return false;
-  if (isNaN(product.price) && product.price > 0) return false;
-  if (isNaN(product.quantity)) return false;
-  return true;
+function _validateProduct(product) {
+  return (
+    typeof product.title == 'string' &&
+    product.title.trim() != '' &&
+    !isNaN(product.price) &&
+    product.price > 0 &&
+    !isNaN(product.quantity) &&
+    Number.isInteger(product.quantity)
+  );
+}
+
+function validId(req, res, next) {
+  if (!isNaN(req.params.id)) {
+    next();
+  } else {
+    const error = new Error('Invalid id');
+    return next(error);
+  }
+}
+
+function getProductFromBody(body) {
+  const { title, description, price, quantity, image } = body;
+
+  return {
+    title,
+    description,
+    price,
+    quantity,
+    image,
+  };
+}
+
+function validProduct(req, res, next) {
+  if (_validateProduct(req.body)) {
+    next();
+  } else {
+    const error = new Error('Invalid product');
+    next(error);
+  }
 }
 
 router.get('/', (req, res) => {
   queries.getAll().then((products) => res.json(products));
 });
 
-router.get('/:id', (req, res, next) => {
-  if (isNaN(req.params.id)) {
-    const error = new Error('Invalid id');
-    return next(error);
-  }
+router.get('/:id', validId, (req, res, next) => {
   queries.getOne(req.params.id).then((product) => {
     if (product) {
       res.json(product);
@@ -29,25 +58,22 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-router.post('/', (req, res, next) => {
-  if (validateProduct(req.body)) {
-    const { title, description, price, quantity, image } = req.body;
+router.post('/', validProduct, (req, res, next) => {
+  const product = getProductFromBody(req.body);
 
-    const product = {
-      title,
-      description,
-      price,
-      quantity,
-      image,
-    };
+  res.json(product);
 
-    queries.create(product).then((id) => {
-      res.json({ id });
-    });
-  } else {
-    const error = new Error('Invalid product');
-    next(error);
-  }
+  // queries.create(product).then((id) => {
+  //   res.json({ id });
+  // });
+});
+
+router.put('/:id', validId, validProduct, (req, res, next) => {
+  const product = getProductFromBody(req.body);
+
+  queries
+    .update(req.params.id, product)
+    .then(() => res.json({ message: 'Updated!' }));
 });
 
 module.exports = router;
